d34f41e0c4b148fd1447bdde07e1d9fac636311b
Timi Tejumola <tejumoladavid@gmail.com>
2022-05-24 23:01:15 +0100

    discord cron job

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(async(req, res, next) => {
 
     console.log("req", req.path)
 
-    if(req.path !== '/cors-anywhere/authorize') {
+    if(req.path !== '/authorize') {
         const token = req?.headers?.authorization?.split(' ')[1];
         console.log("token", token)
         if(!token) {
